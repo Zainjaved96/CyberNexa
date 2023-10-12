90594144bf9a3512f27d61bb944310b47c05636f
Zain <zainjaved965@gmail.com>
2023-10-12 19:21:58 +0500

    fixed process

--- components/Process.tsx
+++ components/Process.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Process = () => {
   return (
     <div >
-    <div className="container mx-auto py-5 bg-base-200 grid cols-2">
+    <div className="row container py-5 bg-base-200 grid cols-2">
       <div>
       <h2 className='text-4xl text-center font-bold'>Our  Streamline <span className="text-secondary"> Process </span></h2>
-        <p>CyberNexa, a leading tech company, follows a three-step approach: Requirements, Analysis, and Solutions. By first thoroughly understanding client needs, we leverage advanced tools to craft effective solutions. Our results consistently surpass expectations, providing clients with tailored, cost-efficient solutions that empower them in the dynamic tech industry.</p>
+        <p className='col-6 max-w-md'>CyberNexa, a leading tech company, follows a three-step approach: Requirements, Analysis, and Solutions. By first thoroughly understanding client needs, we leverage advanced tools to craft effective solutions. Our results consistently surpass expectations, providing clients with tailored, cost-efficient solutions that empower them in the dynamic tech industry.</p>
       </div>
       
         <div className="flex flex-col justify-center py-[4rem] ">
