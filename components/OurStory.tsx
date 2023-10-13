6b60ecf8afaa6a8db8469cb119e473fc24d2c2bb
Zain <zainjaved965@gmail.com>
2023-10-13 06:21:56 +0500

    fixed aspos

--- components/OurStory.tsx
+++ components/OurStory.tsx
@@ -8,7 +8,7 @@ const OurStory = () => {
     <div className="hero-content flex-col  lg:flex-row">
     <div className='flex flex-col gap-1 max-w-[40rem]'>
         <h2 className="text-5xl text-primary font-bold ">Our Story</h2>
-        <p className="col-4 ">In a digital age where innovation is paramount, CyberNexa has emerged as a pioneering force, driven by an unwavering vision to revolutionize the tech landscape. Their groundbreaking cybersecurity system has safeguarded global enterprises and governments, while their cutting-edge solutions have transcended industries, shaping smart cities, enabling healthcare advancements, and fueling sustainable energy practices. Through dedication, resilience, and an exceptional team, CyberNexa's legacy stands as a testament to the limitless possibilities of technology, inspiring a future where innovation knows no bounds.</p>
+        <p className="col-4 ">In a digital age where innovation is paramount, CyberNexa has emerged as a pioneering force, driven by an unwavering vision to revolutionize the tech landscape. Their groundbreaking cybersecurity system has safeguarded global enterprises and governments, while their cutting-edge solutions have transcended industries, shaping smart cities, enabling healthcare advancements, and fueling sustainable energy practices. Through dedication, resilience, and an exceptional team, CyberNexa&apos;s legacy stands as a testament to the limitless possibilities of technology, inspiring a future where innovation knows no bounds.</p>
         <div className="flex gap-2 pt-5">
         <button className="btn btn-primary ">Our Service</button>
         <Link href={'https://calendly.com/iamsajjadali-dev/30min'} className="btn btn-outline btn-secondary ">Get in Touch</Link>
